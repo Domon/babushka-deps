a20d224465f4e4f9ecdfef438f12b51ac72d36ff
Chun-wei Kuo <Dendoh@gmail.com>
2017-04-21 11:25:51 +1000

    Add rbenv-default-gems dep

--- rbenv.rb
+++ rbenv.rb
@@ -1,3 +1,15 @@
 dep 'rbenv', template: 'bin' do
   installs 'rbenv'
 end
+
+dep 'rbenv-default-gems', template: 'managed' do
+  requires 'rbenv'
+
+  def script
+    '`brew --prefix`/etc/rbenv.d/install/default-gems.bash'
+  end
+
+  met? { shell("test -f #{script}") }
+
+  installs 'rbenv-default-gems'
+end
